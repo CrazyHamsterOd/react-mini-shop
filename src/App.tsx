72ca1035c205358bd7457b017231ec4093b04aaa
Yevhen Poliakovskyi <extern.poliakovskyi_yevhen@allianz.com>
2025-10-21 14:52:24 +0300

    feat: add tailwind ccs

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import ProductList from './components/product-list/product-list.tsx';
 
 function App() {
   return (
-    <div style={{ padding: '20px' }}>
+    <div style={{ padding: '20px'}} className="bg-red-500">
       <h1>Mini-shop React</h1>
       <ProductList />
     </div>
